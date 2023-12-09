f2ac47ffa33ab2e8d4311b439966c73749e83bd8
René Jahn <rene.jahn90@gmx.de>
2023-12-09 19:46:09 +0100

    add solution for day 01

---
+++ day_01/day01.py
@@ -0,0 +1,14 @@
+def main():
+    with open('./input.txt') as file:
+        lines = file.readlines()
+        just_numbers = map(only_numbers, lines)
+        calibration_values = [int(f'{nums[0]}{nums[-1]}') for nums in just_numbers]
+        print(sum(calibration_values))
+
+
+def only_numbers(string: str):
+    return [c for c in string if c.isdigit()]
+
+
+if __name__ == '__main__':
+    main()
